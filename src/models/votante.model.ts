60a44f34631ac8cef6b6d2427d26baa389e9411b
Davidrc26 <david.ramirez22171@ucaldas.edu.co>
2022-08-29 19:43:13 -0500

    Se agregan modelos bloque-votante, bloque, votante, bloque-cargo, plancha, cargo

---
+++ src/models/votante.model.ts
@@ -0,0 +1,68 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Votante extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  primerNombre: string;
+
+  @property({
+    type: 'string',
+  })
+  segundoNombre?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  primerApellido: string;
+
+  @property({
+    type: 'string',
+  })
+  segundoApellido?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  documento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  hash: string;
+
+
+  constructor(data?: Partial<Votante>) {
+    super(data);
+  }
+}
+
+export interface VotanteRelations {
+  // describe navigational properties here
+}
+
+export type VotanteWithRelations = Votante & VotanteRelations;
